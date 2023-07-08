ce22e8f96bebdccc923577181abb06574bd3cdaa
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-07-08 23:05:13 +0530

    BOARDING PASSESSS

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,7 +3,8 @@ import HomePage from '../pages/home.f7';
 import NotFoundPage from '../pages/404.f7';
 import AddFlightPage from "../pages/add-flight.f7";
 import FlightPage from "../pages/flight.f7";
-
+import BoardingPassesPage from "../pages/boardingpasses.f7";
+import AddPassPage from '../pages/addpass.f7';
 var routes = [
   {
     path: '/',
@@ -14,8 +15,16 @@ var routes = [
     component: FlightPage,
   },
   {
+    path: "/flight/:flightId/passes",
+    component: BoardingPassesPage,
+  },
+  {
     path: '/add-flight',
     component: AddFlightPage,
+  },
+  {
+    path: '/add-pass',
+    component: AddPassPage,
   },
   {
     path: '(.*)',
