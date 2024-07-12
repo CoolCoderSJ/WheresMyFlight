7f8189a2a6b896b0d77a744cfb84ed0fd2026535
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-07-12 12:46:19 -0400

    start sms notifs setup

---
+++ functions/send-verification/src/main.py
@@ -0,0 +1,36 @@
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+import os, requests, random, json
+
+
+def main(context):
+    client = (
+        Client()
+        .set_endpoint("https://appwrite.shuchir.dev/v1")
+        .set_project("wheresmyflight")
+        .set_key(os.environ["APPWRITE_KEY"])
+    )
+    db = Databases(client)
+
+    req = json.loads(context.req.body)
+    context.log(req)
+
+    userId = req['userId']
+
+    try:
+        code = db.get_document("settings", "phoneVerification", req['userId']+req['phoneNum'])['code']
+    except:
+        code = str(random.randint(100000, 999999))
+        db.create_document("settings", "phoneVerification", req['userId']+req['phoneNum'], {
+            "code": code,
+            "userId": req['userId'],
+            "phoneNum": req['phoneNum']
+        },[
+            f'read("user:{userId}")', 
+            f'write("user:{userId}")'
+        ])
+        requests.post(f"https://text.shuchir.dev/text/{req['phoneNum']}", json={"message": f"Your WheresMyFlight verification code is {code}. Do not share this with anyone."})
+
+    context.log(code)
+
+    return context.res.send(code)
